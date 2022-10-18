cf3300ab3b1e1bdea3655d48c5f38ccdbe3c1cc8
JT <38105420+wjdxor@users.noreply.github.com>
2022-10-18 10:06:07 +0900

    test: orderTest 작성

--- src/test/java/hello/bookstore/fake/FakeBookServiceTest.java
+++ src/test/java/hello/bookstore/fake/FakeBookServiceTest.java
@@ -1,13 +1,20 @@
 package hello.bookstore.fake;
 
 import hello.bookstore.Book;
+import hello.bookstore.BookResponse;
 import hello.bookstore.BookService;
+import hello.bookstore.Discount;
+import hello.bookstore.OrderRequest;
+import hello.bookstore.OrderResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 public class FakeBookServiceTest {
@@ -27,7 +34,7 @@ public class FakeBookServiceTest {
         Book book = new Book(123L,"책이름", "책내용", LocalDateTime.now());
         bookRepository.save(book);
         //then
-        Assertions.assertThat(bookService.findBook(123L).getTitle()).isEqualTo("책이름");
+        assertThat(bookService.findBook(123L).getTitle()).isEqualTo("책이름");
     }
 
     @Test
@@ -37,7 +44,17 @@ public class FakeBookServiceTest {
         bookRepository.save(new Book(2L,"책이름", "책내용", LocalDateTime.now()));
         bookRepository.save(new Book(3L,"책이름", "책내용", LocalDateTime.now()));
         //then
-        Assertions.assertThat(bookService.findBooks().getBookResponses().size()).isEqualTo(3);
+        assertThat(bookService.findBooks().getBookResponses().size()).isEqualTo(3);
     }
 
+    @Test
+    void orderBookTest() {
+        //given
+        bookRepository.save(new Book(1L,"책이름", "책내용", LocalDateTime.now()));
+        OrderRequest request = new OrderRequest(1L,1L,1L, List.of(Discount.TEN_PERCENT));
+        //when
+        OrderResponse response = bookService.order(request);
+        //then
+        assertThat(response.getPrice()).isEqualTo(90L);
+    }
 }
